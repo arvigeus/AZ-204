3ee7a063340410cc1ab1a66f28ed615789457b96
gc674 <g.coman@hotmail.com>
2025-11-16 21:39:03 +0200

    answers are accessible now

--- quiz-app/app/components/RichMarkdown.tsx
+++ quiz-app/app/components/RichMarkdown.tsx
@@ -78,66 +78,66 @@ export const RichMarkdown = ({ interactive, children }: RichMarkdownProps) => {
 					   );
 				   },
 				   code({ className, children, ...props }) {
-					const match = /language-(\w+)/.exec(className || '');
+					   const match = /language-(\w+)/.exec(className || '');
 
-					if (!match)
-						return (
-							<code {...props} className={className}>
-								{children}
-							</code>
-						);
+					   if (!match)
+						   return (
+							   <code {...props} className={clsx(className, 'text-[var(--color-text)]')}>
+								   {children}
+							   </code>
+						   );
 
-					let language = match[1] || '';
-					const code = `${String(children).replace(/\n$/, '')}\n`;
+					   let language = match[1] || '';
+					   const code = `${String(children).replace(/\n$/, '')}\n`;
 
-					// Fix language for highlighter
-					switch (language) {
-						case 'Dockerfile':
-							language = 'docker';
-							break;
-						default:
-							break;
-					}
+					   // Fix language for highlighter
+					   switch (language) {
+						   case 'Dockerfile':
+							   language = 'docker';
+							   break;
+						   default:
+							   break;
+					   }
 
-					const highlightedCode = (
-						<div className="pt-[0.6rem]">
-							<SyntaxHighlighter
-								style={ghcolors}
-								language={language}
-								wrapLongLines
-								codeTagProps={{
-									className:
-										'text-[14px] leading-[1.38] font-[monospace] px-[1.1rem] block text-nowrap! text-[var(--color-text)] dark:text-white',
-								}}
-							>
-								{`${code}\n\n`}
-							</SyntaxHighlighter>
-						</div>
-					);
+					   const highlightedCode = (
+						   <div className="pt-[0.6rem]">
+							   <SyntaxHighlighter
+								   style={ghcolors}
+								   language={language}
+								   wrapLongLines
+								   codeTagProps={{
+									   className:
+										   'text-[14px] leading-[1.38] font-[monospace] px-[1.1rem] block text-nowrap! text-[var(--color-text)]',
+								   }}
+							   >
+								   {`${code}\n\n`}
+							   </SyntaxHighlighter>
+						   </div>
+					   );
 
-					// Only render CodeEditor on client after component is loaded
-					if (CodeEditor) {
-						if (interactive && isLanguageEditSupported(language)) {
-							return (
-								<>
-									<CodeEditor value={`${code}\n`} lang={language} />
+					   // Only render CodeEditor on client after component is loaded
+					   if (CodeEditor) {
+						   if (interactive && isLanguageEditSupported(language)) {
+							   return (
+								   <>
+									   <CodeEditor value={`${code}\n`} lang={language} />
 
-									<div className="mt-4 text-[var(--color-text)] text-xs italic">
-										Note: On the real exam you don't have to write code
-									</div>
-								</>
-							);
-						}
+									   <div className="mt-4 text-[var(--color-text)] text-xs italic">
+										   Note: On the real exam you don't have to write code
+									   </div>
+								   </>
+							   );
+						   }
 
-						if (isLanguageSupported(language)) {
-							return (
-								<CodeEditor readOnly value={`${code}\n`} lang={language} />
-							);
-						}
-					}
+						   if (isLanguageSupported(language)) {
+							   return (
+								   <CodeEditor readOnly value={`${code}\n`} lang={language} />
+							   );
+						   }
+					   }
 
-					return highlightedCode;
-				},
+					   return highlightedCode;
+				   },
 			}}
 		>
 			{children}
